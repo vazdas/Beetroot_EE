ce1a0c39ee87051a8f9e16d9f59f9cef037ff9d0
vdemianchuk <vdemianchuk@n-ix.com>
2026-08-07 10:03:08 +0300

    added PoliceLight project

---
+++ ESP32_PoliceLIght/ESP32_PoliceLIght/src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+#define LED_RED 15
+#define LED_BLUE 9
+
+int speed = 200;
+
+void setup() {
+  Serial.begin(115200);
+  delay(2000);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BLUE,HIGH);
+  digitalWrite(LED_RED,LOW);
+  delay(speed);
+  digitalWrite(LED_BLUE,LOW);
+  digitalWrite(LED_RED,HIGH);
+  delay(speed);
+}
