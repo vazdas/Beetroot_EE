ee08decceef9b8e65aa38c6b9a1277496927be1f
vdemianchuk <vdemianchuk@n-ix.com>
2026-08-10 01:41:43 +0300

    checking Buttons status

--- ESP32_Button/ESP32_Button/src/main.cpp
+++ ESP32_Button/ESP32_Button/src/main.cpp
@@ -3,25 +3,34 @@
 #define BUTTON_IN 16
 #define LED_OUT   15
 
+int lastButtonState = LOW;
+
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
   pinMode(LED_OUT, OUTPUT);
-  pinMode(BUTTON_IN, INPUT_PULLUP);
+  pinMode(BUTTON_IN, INPUT_PULLDOWN);
   digitalWrite(LED_OUT, LOW);
 }
 
 void loop() {
-  int buttonState = digitalRead(BUTTON_IN);
+  int currentState = digitalRead(BUTTON_IN);
 
-  if (buttonState == HIGH) {
-    digitalWrite(LED_OUT, HIGH);
-    Serial.println("Button pressed");
-  } else {
-    digitalWrite(LED_OUT, LOW);
-    Serial.println("Button released");
+  //checking whether the status was changed
+  if (currentState != lastButtonState) {
+    if (currentState == HIGH) {
+      digitalWrite(LED_OUT, HIGH);
+      Serial.println("Button pressed");
+    } else {
+      digitalWrite(LED_OUT, LOW);
+      Serial.println("Button released");
+    }
+
+    delay(100);
+
+    //updateing current status
+    lastButtonState = currentState;
+
   }
-
-  delay(100);
 }
