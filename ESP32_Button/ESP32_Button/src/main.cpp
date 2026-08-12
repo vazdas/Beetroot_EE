fe45bff244d1b9f05bf75199128176885da23cf1
vdemianchuk <vdemianchuk@n-ix.com>
2026-08-13 01:00:54 +0300

    added LedBlinkingThreeModes project

--- ESP32_Button/ESP32_Button/src/main.cpp
+++ ESP32_Button/ESP32_Button/src/main.cpp
@@ -1,36 +1,68 @@
 #include <Arduino.h>
 
-#define BUTTON_IN 16
-#define LED_OUT   15
+#define EXT_BUTTON 18 //external button
+#define BOOT_BUTTON 0 //boot button
+#define LED_RED   15
+#define LED_GREEN 16
 
-int lastButtonState = LOW;
+uint32_t blinkDelay = 400; //delay for LED blinking
+
+void LedBlinking(uint32_t delayMs); //function for LED blinking
+
+
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  pinMode(LED_OUT, OUTPUT);
-  pinMode(BUTTON_IN, INPUT_PULLDOWN);
-  digitalWrite(LED_OUT, LOW);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+
+  pinMode(EXT_BUTTON, INPUT_PULLDOWN);
+  pinMode(BOOT_BUTTON, INPUT_PULLUP);
 }
 
+
+
 void loop() {
-  int currentState = digitalRead(BUTTON_IN);
+  //read the button's state
+  int currentExtState = digitalRead(EXT_BUTTON);
+  int currentBootState = digitalRead(BOOT_BUTTON);
 
-  //checking whether the status was changed
-  if (currentState != lastButtonState) {
-    if (currentState == HIGH) {
-      digitalWrite(LED_OUT, HIGH);
-      Serial.println("Button pressed");
-    } else {
-      digitalWrite(LED_OUT, LOW);
-      Serial.println("Button released");
-    }
 
-    delay(100);
-
-    //updateing current status
-    lastButtonState = currentState;
-
+  //below blocks compare the state of the buttons
+  if (currentExtState == HIGH && currentBootState == LOW) {
+    blinkDelay = 400; //delay for normal blinking
+    Serial.println("Blinking mode: NORMAL");
+    delay(50);        //delay to avoid debounce
   }
+
+
+  if (currentExtState == HIGH && currentBootState == HIGH) {
+    blinkDelay = 50; //delay for fast blinking
+    Serial.println("Blinking mode: FAST");
+    delay(50);        //delay to avoid debounce
+  }
+  
+  
+  if (currentBootState == LOW && currentExtState == LOW) {
+    blinkDelay = 700; //delay for slow blinking
+    Serial.println("Blinking mode: SLOW");
+    delay(50);        //delay to avoid debounce
+  }
+
+  //the main logic
+  LedBlinking(blinkDelay);
+}
+
+
+
+void LedBlinking(uint32_t delayMs) {
+  digitalWrite(LED_RED, HIGH);
+  digitalWrite(LED_GREEN, LOW);
+  delay(delayMs);
+
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_GREEN, HIGH);
+  delay(delayMs);
 }
