1fc8d78c1905e522651a560464b4f4ed130c2bbe
vdemianchuk <vdemianchuk@n-ix.com>
2026-09-10 09:50:20 +0300

    Update the Version 1

--- ESP32_DebounceVersions/src/main.cpp
+++ ESP32_DebounceVersions/src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define BUTTON_PIN 15
+
+volatile uint32_t g_interruptCount = 0;
+volatile bool g_isrFlag = false;
+
+void IRAM_ATTR buttonIsr() {
+    g_interruptCount++;
+    g_isrFlag = true;
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonIsr, FALLING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (g_isrFlag) {
+        g_isrFlag = false;
+        Serial.printf("Raw Interrupt! Total ISR calls: %u\n", g_interruptCount);
+    }
 }
