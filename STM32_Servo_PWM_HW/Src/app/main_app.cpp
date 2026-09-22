0e9857abcc9151aeef5e268902545511fc718f8d
vdemianchuk <vdemianchuk@n-ix.com>
2026-09-23 02:35:36 +0300

    added project: STM32_Servo_PWM_HW

--- STM32_Servo_PWM_HW/Src/app/main_app.cpp
+++ STM32_Servo_PWM_HW/Src/app/main_app.cpp
@@ -5,9 +5,11 @@
 #include "pwm/pwm.h"
 #include "servo/servo.h"
 
+// Access to the ADC variable
+extern ADC_HandleTypeDef hadc1;
+
 #define SERVO_GPIO_PORT PWM_PORT_B
 #define SERVO_GPIO_PIN 4
-#define SERVO_STEP_ANGLE 10
 
 extern "C" void main_cpp() {
     bool error = false;
@@ -29,20 +31,30 @@ extern "C" void main_cpp() {
     while (1) {
         if (error) {
             printf("Init error\n");
-        } else {
-            for (uint16_t angle = SERVO_MIN_ANGLE;
-                 angle <= SERVO_MAX_ANGLE; angle += SERVO_STEP_ANGLE) {
-                Servo_SetAngle(&servo, angle);
-                HAL_Delay(100);
-            }
-
-            for (int angle = SERVO_MAX_ANGLE; angle >= SERVO_MIN_ANGLE;
-                 angle -= SERVO_STEP_ANGLE) {
-                Servo_SetAngle(&servo, (uint16_t)angle);
-                HAL_Delay(100);
-            }
+            HAL_Delay(1000);
+            continue;
         }
 
-        HAL_Delay(500);
+        // Reading ADC
+        HAL_ADC_Start(&hadc1);
+        HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+        uint32_t adc_raw = HAL_ADC_GetValue(&hadc1);
+        HAL_ADC_Stop(&hadc1);
+
+        // Calculating the angle for potenciometer and servo
+        uint32_t pot_angle = (adc_raw * 270) / 4095;
+        uint16_t target_angle = (uint16_t)pot_angle;
+
+        // Cutting off excess angle values
+        if (target_angle > SERVO_MAX_ANGLE) {
+            target_angle = SERVO_MAX_ANGLE;
+        }
+
+        // Aplying the result and printing to the terminal
+        Servo_SetAngle(&servo, target_angle);
+        printf("RAW data: %lu\nPotentiometer angle: %lu\nServo angle: %u\n", adc_raw, pot_angle, target_angle);
+
+        HAL_Delay(50);
     }
 }
+
